d3c917ca1f281f623ec361f78cac1f9cd14e80e8
me_zizi <mailtmn11@gmail.com>
2019-01-29 19:31:46 +0300

    hw-5 add spinner to feed s  state

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -16,19 +16,22 @@ export default class Feed extends Component {
             { id: '123', comment: 'Hi there!', created: 1526825076849 },
             { id: '177', comment: 'Hello !', created: 1526825076849 },
         ],
+        spinnerState: false,
     };
 
     render() {
-        const { posts } = this.state;
+        const { posts, spinnerState } = this.state;
+
         const postsJSX = posts.map((post) => {
-            return <Post key = { post.id } { ...post }/>;
+            // eslint-disable-next-line react/jsx-max-props-per-line
+            return <Post key = { post.id } { ...post } />;
         });
 
         return (
             <section className = { Styles.feed }>
-                <Spinner isSpinning/>
-                <StatusBar/>
-                <Composer/>
+                <Spinner isSpinning = { spinnerState } />
+                <StatusBar />
+                <Composer />
                 {postsJSX}
             </section>
         );
