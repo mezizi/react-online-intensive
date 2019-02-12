04555025e9b679d12aa6556d041b1d3cbd4eb075
me_zizi <mailtmn11@gmail.com>
2019-02-12 13:45:17 +0300

    react-online-lesson-7

--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -5,11 +5,12 @@ import { func, string, number, array } from 'prop-types';
 
 //Components
 import Like from 'components/Like';
-import { Consumer } from 'components/HOC/withProfile';
+import { withProfile } from 'components/HOC/withProfile';
 
 //Instruments
 import Styles from './styles.m.css';
 
+@withProfile
 export default class Post extends Component {
     static propTypes = {
         _likePost:   func.isRequired,
@@ -29,31 +30,27 @@ export default class Post extends Component {
 
     render() {
         const { comment, created, _likePost, id, likes } = this.props;
+        const { avatar, currentUserFirstName, currentUserLastName } = this.props;
 
         return (
-            <Consumer>
-                {(context) => (
-                    <section className = { Styles.post }>
-                        <span
-                            className = { Styles.cross }
-                            onClick = { this._removePost }
-                        />
-                        <img src = { context.avatar } />
-                        <a>
-                            {`${context.currentUserFirstName} 
-                            ${context.currentUserLastName}`}
-                        </a>
-                        <time>{moment.unix(created).format('MMM D h:mm:ss a')}</time>
-                        <p>{comment}</p>
-                        <Like
-                            _likePost = { _likePost }
-                            id = { id }
-                            likes = { likes }
-                            { ...context }
-                        />
-                    </section>
-                )}
-            </Consumer>
+            <section className = { Styles.post }>
+                <span
+                    className = { Styles.cross }
+                    onClick = { this._removePost }
+                />
+                <img src = { avatar } />
+                <a>
+                    {`${currentUserFirstName} 
+                    ${currentUserLastName}`}
+                </a>
+                <time>{moment.unix(created).format('MMM D h:mm:ss a')}</time>
+                <p>{comment}</p>
+                <Like
+                    _likePost = { _likePost }
+                    id = { id }
+                    likes = { likes }
+                />
+            </section>
         );
     }
 }
